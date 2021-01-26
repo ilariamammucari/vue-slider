74e6607625bf96de8fb02ae51f0bd46a0c9f7576
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-26 14:51:03 +0100

    prova setInterval

--- vue/script.js
+++ vue/script.js
@@ -1,4 +1,4 @@
-// ! slide foto con Vue.js
+// ! slider foto con Vue.js
 
 var app = new Vue({
     el: '#root',
@@ -9,20 +9,27 @@ var app = new Vue({
             'img/img2.jpg',
             'img/img3.jpg'
         ],
-        contatore: 0
+        contatore: 0,
+        sliderAuto: ''
+    },
+    created: function() {
+        // sliderAuto = setInterval(this.prossimaFoto, 2000);
     },
     methods: {
         // * funzione per scorrere foto in avanti
         prossimaFoto() {
             ( this.contatore == this.immagini.length - 1 ) ? this.contatore = 0 : this.contatore++;
+            clearInterval(this.sliderAuto);
         },
         // * funzione per scorrere foto indietro
         fotoPrecedente() {
             ( this.contatore == 0 ) ? this.contatore = this.immagini.length - 1 : this.contatore--;
+            clearInterval(this.sliderAuto);
         },
-        // * cambio img cliccando sul pallino
+        // * funzione cambio img cliccando sul pallino
         cambioImg(index) {
             this.contatore = index;
+            clearInterval(this.sliderAuto);
         }
     }
 });
