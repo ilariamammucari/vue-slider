5d3758374f46dac1215cd672e4f29e7067419333
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-26 14:21:18 +0100

    slider con Vue.js

--- vue/script.js
+++ vue/script.js
@@ -1,1 +1,28 @@
-alert();
+// ! slide foto con Vue.js
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        immagini: [
+            'img/img0.jpg',
+            'img/img1.jpg',
+            'img/img2.jpg',
+            'img/img3.jpg'
+        ],
+        contatore: 0
+    },
+    methods: {
+        // * funzione per scorrere foto in avanti
+        prossimaFoto() {
+            ( this.contatore == this.immagini.length - 1 ) ? this.contatore = 0 : this.contatore++;
+        },
+        // * funzione per scorrere foto indietro
+        fotoPrecedente() {
+            ( this.contatore == 0 ) ? this.contatore = this.immagini.length - 1 : this.contatore--;
+        },
+        // * cambio img cliccando sul pallino
+        cambioImg(index) {
+            this.contatore = index;
+        }
+    }
+});
